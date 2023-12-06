b0e6fe614695ce9337b6e5f6bb8251f9704edf8a
Bhavya Kumar Singh <bhavyakumar960@gmail.com>
2023-12-06 12:49:13 +0530

    Update Main.java

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -3,74 +3,36 @@ package com.main;
 public class Main {
 	 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		throw new RuntimeException("kjhg");
+		
+		int a = 11; int b = 22; int c;
+		
+		c  = a + b + a++ + b++ + ++a + ++b;
+		
+		
+		
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+		a = 11; b = 22; c = 0;
+		
+		 c = a--;
+		 System.out.println(c);
+		 System.out.println(a);
 	}
 	
 }
 
 /*
- * 							OOPS based on PIE
- * P - Polymorphism
- * I - Inheritance
- * E - encapsulation
- * A - Abstraction 
+ * Increment
+ * 
+ *  Pre - ++a
+ *  Post - a++
+ * 
+ * 
+ * Decrement 
+ * Pre - --a
+ * Post - a--
  * 
  * */
-
-
-/*
- * IDETIFIERS name given to any java program. a to z, A to Z, 0-9, $, _
- * 
- * Lenght of identifiers - infinite 
- * case-sensitive
- * 
- * 
- * reserve keywords
- * 53 -> 50(keywords) + 3(Literal)
- * 50(keywords)-> 48(used) + 2(goto, const)
- * 	48 includes - Access modifiers + datATYPES, flow control, exception handing + class level keywords + object
- * 
- * datatypes -> byte, short, char, int, long, float, double, boolean(primitive types)
- * 
- * 	3 literals - > void, true, false
- * 
- * Enums : 
- * 
- * enum A{
- * 	TERMINATED, SUSPENDED
- * }
- * 
- * Methods, attributes -> camelCase  -- doItEarly
- * class, interface -> Pascal ---DoItEarly
- * 
- * Scenerios where java do not use camelCase
- * instanceof, strictfp
- * 
- * 
- * Is java pure object oriented or not ? (No)
- * 1. support primitives
- * 2. multiple inheritance not support in class
- * 
- * Multiple inheritance is suppoted in case of interfaces
- * 
- * 
- * interface
- * 
- * interface Bank{
- * 		String address, id;
- * 
- * 		setInterest, getInterest, loan
- * }
- * 
- * class PunjabNationalBank implements Bank{
- * 	
- * }
- * 	
- *	 	byte b = 10;
- * 		b++; ->  b = b + 1 
- * 		sout(b);
- * 
- *  
- * 
- */
